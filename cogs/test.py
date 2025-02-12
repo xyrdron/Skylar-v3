563dc11fabfa1df8b21de59e94217464f3e01354
skyy dev :3 <imskyy@xyrdron.com>
2025-02-13 09:54:39 +1100

    About
    Signed-off-by: skyy dev :3 <imskyy@xyrdron.com>

--- cogs/test.py
+++ cogs/test.py
@@ -5,9 +5,9 @@ class bottest(commands.Cog):
     self.bot = bot
     self._last_member = None
 
-  @commands.hybrid_command(name="test1", description="test command")
+  @commands.hybrid_command(name="About", description="About")
   async def test1(self,ctx):
-    await ctx.send("pong")
+    await ctx.send("heyoooo skylar fans!! I know your excited but the bot is still WIP and will be done soon okiee?? i hope to have fun with yall soon, luv yall :D")
 
 def setup(client):
   return client.add_cog(bottest(client))
