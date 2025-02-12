ba28d6816e6482839070c4775985388952d097af
skyy dev :3 <imskyy@xyrdron.com>
2025-02-13 10:03:34 +1100

    im so dumb
    Signed-off-by: skyy dev :3 <imskyy@xyrdron.com>

--- cogs/test.py
+++ cogs/test.py
@@ -5,7 +5,7 @@ class bottest(commands.Cog):
     self.bot = bot
     self._last_member = None
 
-  @commands.hybrid_command(name="About", description="About")
+  @commands.hybrid_command(name="about", description="About")
   async def test1(self,ctx):
     await ctx.send("heyoooo skylar fans!! I know your excited but the bot is still WIP and will be done soon okiee?? i hope to have fun with yall soon, luv yall :D")
 
